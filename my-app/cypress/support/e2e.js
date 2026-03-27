e834ece467a5e4b6512686ed749f5c55360cbac6
Kleas <kleas3.marc@gmail.com>
2026-03-27 13:01:27 +0100

    act 3

--- my-app/cypress/support/e2e.js
+++ my-app/cypress/support/e2e.js
@@ -1,1 +1,9 @@
-// Custom commands can be imported here.
+const apiUrl = Cypress.env('apiUrl') || 'http://localhost:8000';
+
+Cypress.Commands.add('resetTestData', (payload = {}) =>
+  cy.request('POST', `${apiUrl}/test/reset`, payload)
+);
+
+Cypress.Commands.add('queueCreateUserFault', (statusCode = 500) =>
+  cy.request('POST', `${apiUrl}/test/faults/next-create-user`, { statusCode })
+);
